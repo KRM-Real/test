86184630420277ea25db489cc40399e9bf57e45c
Krm.dev <144045555+KRM-Real@users.noreply.github.com>
2025-06-13 17:05:10 +0800

    Delete test-project directory

--- test-project/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeeController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/index', [EmployeeController::class, 'index']);
